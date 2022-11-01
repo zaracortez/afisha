4bdfc85eda5d478811f804b7179b83383ed1cf22
Zara116 <zarema.mindubaeva@yandex.ru>
2022-11-01 23:14:40 +0300

    Initial commit

---
+++ src/main/java/ru/netology/afisha/Manager.java
@@ -0,0 +1,43 @@
+package ru.netology.afisha;
+
+public class Manager {
+
+    private String[] movies = new String[0];
+    private int limit;
+
+    public Manager() {
+        limit = 10;
+    }
+
+    public Manager(int limit) {
+        this.limit = limit;
+    }
+
+    public void add(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+    }
+
+    public String[] findAll() {
+        return movies;
+    }
+
+    public String[] findLast() {
+        int resultSize;
+        if (limit < movies.length) {
+            resultSize = limit;
+        } else {
+            resultSize = movies.length;
+        }
+        String[] result = new String[resultSize];
+        for (int i = 0; i < resultSize; i++) {
+            result[i] = movies[movies.length - 1 - i];
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/netology/afisha/ManagerTest.java
@@ -0,0 +1,157 @@
+package ru.netology.afisha;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ManagerTest {
+    @Test
+    public void testAddedAll() {
+        String movie1 = "movie1";
+        String movie2 = "movie2";
+        String movie3 = "movie3";
+        String movie4 = "movie4";
+        String movie5 = "movie5";
+        Manager manager = new Manager();
+        manager.add(movie1);
+        manager.add(movie2);
+        manager.add(movie3);
+        manager.add(movie4);
+        manager.add(movie5);
+
+        String[] expected = {
+                movie1,
+                movie2,
+                movie3,
+                movie4,
+                movie5,
+        };
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddedLastInLimit() {
+        String movie1 = "movie1";
+        String movie2 = "movie2";
+        String movie3 = "movie3";
+        String movie4 = "movie4";
+        String movie5 = "movie5";
+        Manager manager = new Manager(5);
+        manager.add(movie1);
+        manager.add(movie2);
+        manager.add(movie3);
+        manager.add(movie4);
+        manager.add(movie5);
+
+        String[] expected = {
+                movie5,
+                movie4,
+                movie3,
+                movie2,
+                movie1,
+        };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddedLastUnderLimit() {
+        String movie1 = "movie1";
+        String movie2 = "movie2";
+        String movie3 = "movie3";
+        String movie4 = "movie4";
+        String movie5 = "movie5";
+        Manager manager = new Manager();
+        manager.add(movie1);
+        manager.add(movie2);
+        manager.add(movie3);
+        manager.add(movie4);
+        manager.add(movie5);
+
+        String[] expected = {
+                movie5,
+                movie4,
+                movie3,
+                movie2,
+                movie1,
+        };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddedLastOnLimit() {
+        String movie1 = "movie1";
+        String movie2 = "movie2";
+        String movie3 = "movie3";
+        String movie4 = "movie4";
+        String movie5 = "movie5";
+        Manager manager = new Manager(10);
+        manager.add(movie1);
+        manager.add(movie2);
+        manager.add(movie3);
+        manager.add(movie4);
+        manager.add(movie5);
+
+        String[] expected = {
+                movie5,
+                movie4,
+                movie3,
+                movie2,
+                movie1,
+        };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddedLastAboveBorderOnLimit() {
+        String movie1 = "movie1";
+        String movie2 = "movie2";
+        String movie3 = "movie3";
+        String movie4 = "movie4";
+        String movie5 = "movie5";
+        Manager manager = new Manager(11);
+        manager.add(movie1);
+        manager.add(movie2);
+        manager.add(movie3);
+        manager.add(movie4);
+        manager.add(movie5);
+
+        String[] expected = {
+                movie5,
+                movie4,
+                movie3,
+                movie2,
+                movie1,
+        };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddedLastLessBorderOnLimit() {
+        String movie1 = "movie1";
+        String movie2 = "movie2";
+        String movie3 = "movie3";
+        String movie4 = "movie4";
+        String movie5 = "movie5";
+        Manager manager = new Manager(1);
+        manager.add(movie1);
+
+
+        String[] expected = {
+
+
+                movie1,
+        };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+
+
+
+
+}
