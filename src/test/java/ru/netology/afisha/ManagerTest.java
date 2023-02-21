f4d1ec8c53c5edbf2c1f1faa318736bd9810c04d
zaracortez <zarema.cortez@yandex.ru>
2023-02-21 18:11:57 +0300

    added tests with limits

--- src/test/java/ru/netology/afisha/ManagerTest.java
+++ src/test/java/ru/netology/afisha/ManagerTest.java
@@ -264,4 +264,109 @@ public class ManagerTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void testFindLastCustomLimit5() {// Если лимит меньше количества фильмов в менеджере
+        Manager manager = new Manager(5);
+        String movie1 = "movie1";
+        String movie2 = "movie2";
+        String movie3 = "movie3";
+        String movie4 = "movie4";
+        String movie5 = "movie5";
+        String movie6 = "movie6";
+        String movie7 = "movie7";
+        String movie8 = "movie8";
+        String movie9 = "movie9";
+
+        manager.add(movie1);
+        manager.add(movie2);
+        manager.add(movie3);
+        manager.add(movie4);
+        manager.add(movie5);
+        manager.add(movie6);
+        manager.add(movie7);
+        manager.add(movie8);
+        manager.add(movie9);
+
+
+        String[] expected = {
+                movie9,
+                movie8,
+                movie7,
+                movie6,
+                movie5,
+
+        };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindLastCustomLimitEqualArray() { // Если лимит равен количеству фильмов в менеджере
+        Manager manager = new Manager(10);
+        String movie1 = "movie1";
+        String movie2 = "movie2";
+        String movie3 = "movie3";
+        String movie4 = "movie4";
+        String movie5 = "movie5";
+        String movie6 = "movie6";
+        String movie7 = "movie7";
+        String movie8 = "movie8";
+        String movie9 = "movie9";
+        String movie10 = "movie10";
+
+        manager.add(movie1);
+        manager.add(movie2);
+        manager.add(movie3);
+        manager.add(movie4);
+        manager.add(movie5);
+        manager.add(movie6);
+        manager.add(movie7);
+        manager.add(movie8);
+        manager.add(movie9);
+        manager.add(movie10);
+
+
+        String[] expected = {
+                movie10,
+                movie9,
+                movie8,
+                movie7,
+                movie6,
+                movie5,
+                movie4,
+                movie3,
+                movie2,
+                movie1,
+        };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindLastIfArrayLowThanLimit() { //Если лимит больше чем фильмов в менеджере
+        Manager manager = new Manager(10);
+        String movie1 = "movie1";
+        String movie2 = "movie2";
+        String movie3 = "movie3";
+        String movie4 = "movie4";
+        String movie5 = "movie5";
+
+
+        manager.add(movie1);
+        manager.add(movie2);
+        manager.add(movie3);
+        manager.add(movie4);
+        manager.add(movie5);
+
+
+        String[] expected = {
+                movie5,
+                movie4,
+                movie3,
+                movie2,
+                movie1,
+        };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
